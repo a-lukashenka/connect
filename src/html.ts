959aef0409c4f839b2d8e2b9beb254b1be8f40ce
Aliaksander Lukashenka <a.lukashenko@aisnovations.com>
2019-05-31 17:06:08 +0300

    images

--- src/html.ts
+++ src/html.ts
@@ -1,5 +1,5 @@
-export const TEMPLATE = '<div id="via-connect__form-wrapper">\n' +
-    '    <div id="via-connect__header-wrapper">\n' +
+export const TEMPLATE = '<div id="via-connect__header-wrapper" class="header">\n' +
+    '      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="#fff" d="M0 1v16.981h4v5.019l7-5.019h13v-16.981h-24zm7 10c-.828 0-1.5-.671-1.5-1.5s.672-1.5 1.5-1.5c.829 0 1.5.671 1.5 1.5s-.671 1.5-1.5 1.5zm5 0c-.828 0-1.5-.671-1.5-1.5s.672-1.5 1.5-1.5c.829 0 1.5.671 1.5 1.5s-.671 1.5-1.5 1.5zm5 0c-.828 0-1.5-.671-1.5-1.5s.672-1.5 1.5-1.5c.829 0 1.5.671 1.5 1.5s-.671 1.5-1.5 1.5z"/></svg>\n' +
     '      <p id="header-title"></p>\n' +
     '    </div>\n' +
     '    <div id="via-connect__form-body">\n' +
@@ -9,19 +9,21 @@ export const TEMPLATE = '<div id="via-connect__form-wrapper">\n' +
     '      <form id="via-connect__form" autocomplete="off">\n' +
     '        <div id="via-connect__message-wrapper" class="right">\n' +
     '          <div class="group">\n' +
-    '      <input type="text" name="name" required="required"/><span class="highlight"></span><span class="bar"></span>\n' +
-    '      <label>Name</label>\n' +
-    '    </div>\n' +
+    '            <input type="text" name="name" required="required"><span class="highlight"></span><span class="bar"></span>\n' +
+    '            <label>Name</label>\n' +
+    '          </div>\n' +
     '          <div class="group">\n' +
-    '      <input type="number" name="phone" required="required"/><span class="highlight"></span><span class="bar"></span>\n' +
-    '      <label>Mobile phone</label>\n' +
-    '    </div>\n' +
+    '            <input type="number" name="phone" required="required"><span class="highlight"></span><span class="bar"></span>\n' +
+    '            <label>Mobile phone</label>\n' +
+    '          </div>\n' +
     '          <div class="group">\n' +
-    '      <textarea type="textarea" name="message" rows="1" required="required"></textarea><span class="highlight"></span><span class="bar"></span>\n' +
-    '      <label>Message</label>\n' +
-    '    </div>\n' +
+    '            <textarea type="textarea" name="message" rows="1" required="required"></textarea><span class="highlight"></span><span class="bar"></span>\n' +
+    '            <label>Message</label>\n' +
+    '          </div>\n' +
     '        </div>\n' +
     '        <button type="submit" class="btn-box"><a class="btn btn-link">Send</a></button>\n' +
     '      </form>\n' +
     '    </div>\n' +
-    '  </div>';
+    '  <div id="via-connect__header-wrapper">\n' +
+    '      <p id="footer-title">Povered by via</p>\n' +
+    '    </div>';
