c128d8a72ccd611e9ebcffb12d4085ab33de3508
Aliaksander Lukashenka <a.lukashenko@aisnovations.com>
2019-05-31 10:37:15 +0300

    form added

--- src/form.ts
+++ src/form.ts
@@ -1,73 +1,28 @@
+import { TEMPLATE } from './html';
+
 export class Form {
     createForm(): void {
         // container
         const container = document.createElement('div');
         container.setAttribute('id', 'via-connect');
+        container.innerHTML = TEMPLATE;
+        document.body.appendChild(container);
 
-        // form wrapper
-        const formWrapper = document.createElement('div');
-        formWrapper.setAttribute('id', 'via-connect__form-wrapper');
+        const headTitle = document.getElementById('header-title');
+        const messageTitle = document.getElementById('message-title');
+        const headTitleMessage = document.createTextNode('We\'ll text you!!');
+        const messageText = document.createTextNode('Enter  your information below and our team will text you shortly!!');
 
-        // form header
-        const header = document.createElement('div');
-        header.setAttribute('id', 'via-connect__header-wrapper');
-
-        const headTitle = document.createElement('p');
-        const headTitleMessage = document.createTextNode('We\'ll text you');
-
-        // form body
-        const formBody = document.createElement('div');
-        formBody.setAttribute('id', 'via-connect__form-body');
-
-        // message wrapper left
-        const messageWrapper1 = document.createElement('div');
-        const infoMessage = document.createElement('p');
-        const infoMessageText = document.createTextNode('Enter  your information below and our team will text you shortly.');
-        messageWrapper1.setAttribute('id', 'via-connect__message-wrapper');
-        messageWrapper1.classList.add('blue');
-
-        // message wrapper right
-        const messageWrapper2 = document.createElement('div');
-        messageWrapper2.setAttribute('id', 'via-connect__message-wrapper');
-        messageWrapper2.classList.add('right');
-
-
-        // form
-        const form = document.createElement('form');
-        form.setAttribute('id', 'via-connect__form');
-        form.setAttribute('method', 'post');
-        form.setAttribute('action', 'submit.php');
-
-        const i = document.createElement('input'); //input element, text
-        i.setAttribute('type', 'text');
-        i.setAttribute('name', 'username');
-
-        const s = document.createElement('input'); //input element, Submit button
-        s.setAttribute('type', 'submit');
-        s.setAttribute('value', 'Submit');
-
-
-        container.appendChild(formWrapper);
-        formWrapper.appendChild(header);
-
-        header.appendChild(headTitle);
+        const form = document.getElementById('via-connect__form');
+        form.onsubmit = function (e: Event) {
+            e.preventDefault();
+            console.log({
+                name: form['name']['value'],
+                phone: form['phone']['value'],
+                message: form['message']['value'],
+            });
+        };
         headTitle.appendChild(headTitleMessage);
-        formWrapper.appendChild(formBody);
-
-        formBody.appendChild(messageWrapper1);
-        formBody.appendChild(messageWrapper2);
-        infoMessage.appendChild(infoMessageText);
-        messageWrapper1.appendChild(infoMessage);
-        messageWrapper2.appendChild(form);
-
-        form.appendChild(i);
-        form.appendChild(s);
-
-        // end form
-
-//and some more input elements here
-//and dont forget to add a submit button
-
-        document.getElementsByTagName('body')[0].appendChild(container);
+        messageTitle.appendChild(messageText);
     }
 }
---
+++ src/html.ts
@@ -0,0 +1,27 @@
+export const TEMPLATE = '<div id="via-connect__form-wrapper">\n' +
+    '    <div id="via-connect__header-wrapper">\n' +
+    '      <p id="header-title"></p>\n' +
+    '    </div>\n' +
+    '    <div id="via-connect__form-body">\n' +
+    '      <div id="via-connect__message-wrapper" class="blue">\n' +
+    '        <p id="message-title"></p>\n' +
+    '      </div>\n' +
+    '      <form id="via-connect__form" autocomplete="off">\n' +
+    '        <div id="via-connect__message-wrapper" class="right">\n' +
+    '          <div class="group">\n' +
+    '      <input type="text" name="name" required="required"/><span class="highlight"></span><span class="bar"></span>\n' +
+    '      <label>Name</label>\n' +
+    '    </div>\n' +
+    '          <div class="group">\n' +
+    '      <input type="number" name="phone" required="required"/><span class="highlight"></span><span class="bar"></span>\n' +
+    '      <label>Mobile phone</label>\n' +
+    '    </div>\n' +
+    '          <div class="group">\n' +
+    '      <textarea type="textarea" name="message" rows="1" required="required"></textarea><span class="highlight"></span><span class="bar"></span>\n' +
+    '      <label>Message</label>\n' +
+    '    </div>\n' +
+    '        </div>\n' +
+    '        <button type="submit" class="btn-box"><a class="btn btn-link">Send</a></button>\n' +
+    '      </form>\n' +
+    '    </div>\n' +
+    '  </div>';
