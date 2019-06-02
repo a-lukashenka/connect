250280d5af09b9b90b59559482038508d71f512f
Aliaksander Lukashenka <aliaksander_lukashenkahotmail.com>
2019-06-02 10:29:35 +0300

    success message added

--- src/form.ts
+++ src/form.ts
@@ -1,45 +1,104 @@
 import { CHAT_TEMPLATE } from './templates/chat-html';
 import { CHAT_BUTTON_TEMPLATE } from './templates/chat-button-html';
+import { SUBMIT_FORM_TEMPLATE } from './templates/submit-form-html';
+import { SUCCESS_FORM_TEMPLATE } from './templates/success-form-html';
 
 export class Form {
     isVisible: boolean = false;
+    isNewRequest: boolean = true;
+    formContainer: HTMLElement;
+    chatContainer: HTMLElement;
+
     createForm(): void {
         // container
-        const container = document.createElement('div');
-        const button = document.createElement('div');
         const body = document.body;
-        container.setAttribute('id', 'via-connect');
+
+        this.chatContainer = document.createElement('div');
+        const button = document.createElement('div');
+
+        this.chatContainer.setAttribute('id', 'via-connect');
         button.setAttribute('id', 'via-connect-btn-container');
-        container.innerHTML = CHAT_TEMPLATE;
-        button.innerHTML = CHAT_BUTTON_TEMPLATE;
+
+        this.chatContainer.innerHTML += CHAT_TEMPLATE;
+        button.innerHTML += CHAT_BUTTON_TEMPLATE;
+
         body.appendChild(button);
-        body.appendChild(container);
+        body.appendChild(this.chatContainer);
 
-        const headTitle = document.getElementById('header-title');
-        const messageTitle = document.getElementById('message-title');
-        const agreementTitle = document.getElementById('via-agreement');
+        button.addEventListener('click', () => {
+            this.isVisible = !this.isVisible;
+            this.setChatView();
+        });
 
+        this.formContainer = document.getElementById('via-connect__form-body');
+        this.setView();
+        this.setTitles();
+        this.setChatView();
+    }
 
-        const headTitleText = document.createTextNode('We\'ll text you!!');
-        const messageText = document.createTextNode('Enter  your information below and our team will text you shortly!!');
-        const agreementText = document.createTextNode('By submitting you agree to receive text message at the number provided. Message/data rates apply.')
+    setView(): void {
+        if (this.isNewRequest) {
+            this.clearSuccessContainer();
+            this.setFormContainer();
+        } else {
+            this.clearFormContainer();
+            this.setSuccessContainer();
+        }
+    }
+
+    setFormContainer(): void {
+        if (!this.formContainer) return;
+        this.formContainer.innerHTML += SUBMIT_FORM_TEMPLATE;
+
+        const agreementTitle = document.getElementById('via-connect__agreement');
+        const agreementText = document.createTextNode('By submitting you agree to receive text message at the number provided. Message/data rates apply.');
+        agreementTitle.appendChild(agreementText);
 
         const form = document.getElementById('via-connect__form');
-        form.onsubmit = function (e: Event) {
+        form.onsubmit = (e: Event) => {
             e.preventDefault();
             console.log({
                 name: form['name']['value'],
                 phone: form['phone']['value'],
                 message: form['message']['value'],
             });
+            this.isNewRequest = false;
+            this.setView();
         };
+    }
 
-        button.addEventListener('click', () => {
-            this.isVisible = !this.isVisible;
-            container.classList.toggle('visible', this.isVisible);
-        });
-        headTitle.appendChild(headTitleText);
-        messageTitle.appendChild(messageText);
-        agreementTitle.appendChild(agreementText);
+    clearFormContainer(): void {
+        const form = document.getElementById('via-connect__form');
+        if (!form) return;
+        form.parentNode.removeChild(form);
+    }
+
+    setSuccessContainer(): void {
+        if (!this.formContainer) return;
+        this.formContainer.innerHTML += SUCCESS_FORM_TEMPLATE;
+    }
+
+    clearSuccessContainer(): void {
+        const form = document.getElementById('via-connect__form');
+        if (!form) return;
+        form.parentNode.removeChild(form);
+    }
+
+    setTitles(): void {
+        const bannerTitle = document.getElementById('via-connect__banner');
+        const initialMessageTitle = document.getElementById('via-connect__initial-message');
+
+
+        const bannerTitleText = document.createTextNode('We\'ll text you!');
+        const initialMessageTitleText = document.createTextNode('Enter  your information below and our team will text you shortly!!');
+
+        bannerTitle.appendChild(bannerTitleText);
+        initialMessageTitle.appendChild(initialMessageTitleText);
+    }
+
+    setChatView(): void {
+        setTimeout(() => this.chatContainer.classList.toggle('visible', this.isVisible), 0);
     }
 }
+
+export const formClass = new Form();
---
+++ src/templates/submit-form-html.ts
@@ -0,0 +1,19 @@
+export const SUBMIT_FORM_TEMPLATE = '<form id="via-connect__form" class="message-wrapper form" autocomplete="off">' +
+    '    <div class="message right clear">' +
+    '      <div class="group">' +
+    '        <input type="text" name="name" required="required"><span class="highlight"></span><span class="bar"></span>' +
+    '        <label>Name</label>' +
+    '      </div>' +
+    '      <div class="group">' +
+    '        <input type="number" name="phone" required="required"><span class="highlight"></span><span class="bar"></span>' +
+    '        <label>Mobile phone</label>' +
+    '      </div>' +
+    '      <div class="group">' +
+    '        <textarea name="message" rows="1" required="required"></textarea>' +
+    '        <span class="highlight"></span><span class="bar"></span>' +
+    '        <label>Message</label>' +
+    '      </div>' +
+    '    </div>' +
+    '    <p id="via-connect__agreement" class="agreement"></p>' +
+    '    <button type="submit" class="btn-box submit"><a class="btn btn-link">Send</a></button>' +
+    '</form>';
